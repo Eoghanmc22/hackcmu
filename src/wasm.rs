cee017074ccf4e5c2107d00b1d509d3ce1bf729e
Eoghanmc22 <eoghanmc@icloud.com>
2025-09-14 01:58:48 -0400

    Same hack as b4 but in our fork of wasmtime instead

--- src/wasm.rs
+++ src/wasm.rs
@@ -473,13 +473,3 @@ impl CompiledCode {
         Ok(())
     }
 }
-
-// Implements thread local storage needed by wasmtime on wasm
-std::thread_local!(static TLS: Cell<*mut u8> = const { Cell::new(std::ptr::null_mut()) });
-pub extern "C" fn tls_get() -> *mut u8 {
-    TLS.with(|p| p.get())
-}
-
-pub extern "C" fn tls_set(ptr: *mut u8) {
-    TLS.with(|p| p.set(ptr));
-}
